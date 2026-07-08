945608b13aeada062cd659456349fa75e2a30325
Коликов Глеб Александрович <gleb.kolikov@example.com>
2026-07-09 02:09:33 +0500

    Создал структуру проекта и настроил сборку через CMake

---
+++ cmake/MyFunctions.cmake
@@ -0,0 +1,7 @@
+function(get_all_sources dir result_var)
+    file(GLOB SOURCE_FILES CONFIGURE_DEPENDS
+        "${dir}/*.c"
+        "${dir}/*.h"
+    )
+    set(${result_var} ${SOURCE_FILES} PARENT_SCOPE)
+endfunction()
